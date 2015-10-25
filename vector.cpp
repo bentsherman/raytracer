43ce6a42c6f4496a942d23605d8f01f88e68b633
Ben Shealy <bentshermann@gmail.com>
2015-10-25 00:06:31 -0400

    Added vector class

---
+++ vector.cpp
@@ -0,0 +1,166 @@
+/**
+ * @file   vector.cpp
+ * @author Ben Shealy
+ *
+ * @section DESCRIPTION
+ *
+ * Implementation of a vector.
+ */
+#include "vector.h"
+#include <math.h>
+
+/**
+ * Construct a vector.
+ *
+ * @param x
+ * @param y
+ * @param z
+ */
+Vector::Vector(double x, double y, double z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+/**
+ * Construct the zero vector.
+ */
+Vector::Vector()
+{
+	this->x = 0;
+	this->y = 0;
+	this->z = 0;
+}
+
+/**
+ * Construct a copy of a vector.
+ *
+ * @param v  vector to copy
+ */
+Vector::Vector(const Vector& v)
+{
+	this->x = v.x;
+	this->y = v.y;
+	this->z = v.z;
+}
+
+/**
+ * Get the x coordinate of a vector.
+ *
+ * @return x coordinate of the vector
+ */
+double Vector::getX() const
+{
+	return this->x;
+}
+
+/**
+ * Get the y coordinate of a vector.
+ *
+ * @return y coordinate of the vector
+ */
+double Vector::getY() const
+{
+	return this->y;
+}
+
+/**
+ * Get the z coordinate of a vector.
+ *
+ * @return z coordinate of the vector
+ */
+double Vector::getZ() const
+{
+	return this->z;
+}
+
+/**
+ * Get the length of a vector.
+ *
+ * @return length of the vector
+ */
+double Vector::length() const
+{
+	return sqrt(this->dot(*this));
+}
+
+/**
+ * Get the sum of two vectors.
+ *
+ * @param rhs  vector to add to this vector
+ * @return     sum of this vector and rhs
+ */
+Vector Vector::operator+(const Vector& rhs) const
+{
+	return Vector(this->x + rhs.x, this->y + rhs.y, this->z + rhs.z);
+}
+
+/**
+ * Get the difference of two vectors.
+ *
+ * @param rhs  vector to subtract from this vector
+ * @return     difference of this vector and rhs
+ */
+Vector Vector::operator-(const Vector& rhs) const
+{
+	return Vector(this->x - rhs.x, this->y - rhs.y, this->z - rhs.z);
+}
+
+/**
+ * Get the product of this vector and a scalar.
+ *
+ * @param c  scalar
+ * @return   product of this vector and c
+ */
+Vector Vector::operator*(double c) const
+{
+	return Vector(c * this->x, c * this->y, c * this->z);
+}
+
+/**
+ * Get the quotient of this vector and a scalar.
+ *
+ * @param c  scalar
+ * @return   quotient of this vector and c
+ */
+Vector Vector::operator/(double c) const
+{
+	return (*this) * (1.0 / c);
+}
+
+/**
+ * Get the unit vector with the same direction as this vector.
+ *
+ * @return unit vector with same direction as this vector
+ */
+Vector Vector::unit() const
+{
+	return (*this) / this->length();
+}
+
+/**
+ * Get the dot product of two vectors.
+ *
+ * @param rhs  vector
+ * @return     dot product of this vector and rhs
+ */
+double Vector::dot(const Vector& rhs) const
+{
+	return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z;
+}
+
+/**
+ * Get the cross product of two vectors.
+ *
+ * @param rhs  vector
+ * @return     cross product of this vector and rhs
+ */
+Vector Vector::cross(const Vector& rhs) const
+{
+	return Vector(
+		this->y * rhs.z - this->z * rhs.y,
+		this->z * rhs.x - this->x * rhs.z,
+		this->x * rhs.y - this->y * rhs.x
+	);
+}
---
+++ vector.h
@@ -0,0 +1,39 @@
+/**
+ * @file   vector.h
+ * @author Ben Shealy
+ *
+ * @section DESCRIPTION
+ *
+ * Interface definition of a vector.
+ */
+#ifndef VECTOR_H
+#define VECTOR_H
+
+// TODO: review operator overloading, also for iostream
+class Vector {
+private:
+	double x;
+	double y;
+	double z;
+
+public:
+	Vector(double x, double y, double z);
+	Vector();
+	Vector(const Vector& v);
+
+	double getX() const;
+	double getY() const;
+	double getZ() const;
+	double length() const;
+
+	Vector operator+(const Vector& rhs) const;
+	Vector operator-(const Vector& rhs) const;
+	Vector operator*(double c) const;
+	Vector operator/(double c) const;
+
+	Vector unit() const;
+	double dot(const Vector& rhs) const;
+	Vector cross(const Vector& rhs) const;
+};
+
+#endif
